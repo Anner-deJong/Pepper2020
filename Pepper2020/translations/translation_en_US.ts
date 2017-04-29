78910ee28a962b3da80c90e5e6b76b9a4391ecfb
Anner <anner.de.jong@outlook.com>
2017-04-30 07:55:16 +0900

    Add files via upload

---
+++ Pepper2020/translations/translation_en_US.ts
@@ -0,0 +1,179 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/After demo</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>That's it! </source>
+            <comment>Text</comment>
+            <translation type="unfinished">That's it! </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Dont rush</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Please don't rush to the train!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Please don't rush to the train!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Dont rush (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>For Pete's sake! Don't rush to the train!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">For Pete's sake! Don't rush to the train!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/HelloWorld</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello world! Pepper at your service.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hello world! Pepper at your service.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Invalid service</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Sorry, no such service is currently being offered. Please try again.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Sorry, no such service is currently being offered. Please try again.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Return to chat</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Pepper here, ready to chat</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Pepper here, ready to chat</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Hello world! Pepper at your service.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello world! Pepper at your service.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Ceasing operations</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Ceasing operations</translation>
+        </message>
+        <message>
+            <source>DIALOG STOPPED</source>
+            <comment>Text</comment>
+            <translation type="obsolete">DIALOG STOPPED</translation>
+        </message>
+        <message>
+            <source>Dialog stopped service $serviceID chosen</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Dialog stopped service $serviceID chosen</translation>
+        </message>
+        <message>
+            <source>Dialog stopped service &quot;$serviceID&quot; chosen</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Dialog stopped service &quot;$serviceID&quot; chosen</translation>
+        </message>
+        <message>
+            <source>Dialog stopped. Service 1 chosen.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Dialog stopped. Service 1 chosen.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (2)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Dialog stopped. Service 2 chosen</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Dialog stopped. Service 2 chosen</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (3)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>resuming main dialog</source>
+            <comment>Text</comment>
+            <translation type="obsolete">resuming main dialog</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Shinto pray</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Then please allow me demonstrate how to pray in a Shinto temple...</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Then please allow me demonstrate how to pray in a Shinto temple...</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Shinto pray (1)</name>
+        <message>
+            <source>Then please allow me demonstrate how to pray in a Shinto temple...</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Then please allow me demonstrate how to pray in a Shinto temple...</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Take pic</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Okay, please let me take your picture then... Say cheeeeze.. 1... 2....3..... cheeeeezu!!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Okay, please let me take your picture then... Say cheeeeze.. 1... 2....3..... cheeeeezu!!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Train rush</name>
+        <message>
+            <source>Dialog stopped. Service 2 chosen</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Dialog stopped. Service 2 chosen</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Please take care.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Please take care.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/maps</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Okay then.. loading the maps service of Google for you...</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Okay then.. loading the maps service of Google for you...</translation>
+        </message>
+    </context>
+</TS>
